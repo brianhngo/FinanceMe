46de52d1a14cba4df169f554f31bb5287479fe8f
Brian Ngo <brianhngo@gmail.com>
2023-11-15 13:34:10 -0800

    boiler plate + Vite + Tailwind set up

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# FinanceMe
+## FrontEnd
+
+React
+HTML
+CSS
+Tailwind
+D3 Data Bisualization
+Typescript
+Javascript
+
+Backend
+Express
+PostgreSQL
+Firebase
+
+Compiler - Vite
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-500"> Hello </h1>
+    </>
+  );
+}
+
+export default App;
