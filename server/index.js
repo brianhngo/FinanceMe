b071833c9b0ae02b09e6ac5f1451003931b3b353
Brian Ngo <brianhngo@gmail.com>
2024-01-08 18:07:39 -0800

    reverted changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const databaseUrl = process.env.DATABASE_URL;
 
 // Extracting port from the database URL or nif ran locally
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Database URL: ${databaseUrl}`);
